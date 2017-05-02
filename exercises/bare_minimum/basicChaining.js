c2bf5b59e4b50ca815c602f7fa2775c4fea0ef1b
jiaconis <john.t.iaconis@gmail.com>
2017-05-02 13:48:03 -0700

    Passes all bare minimum requirement tests

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,13 +10,39 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
-
+var github = require('./promisification.js');
+var files = require('./promiseConstructor.js');
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return files.pluckFirstLineFromFileAsync(readFilePath)
+    .then( (str) => {
+      return github.getGitHubProfileAsync(str);
+    })
+    .then( (result) => {
+      let results = JSON.stringify(result);
+      // fs.writeFileSync(writeFilePath, results, (err) => {
+      //   if (!err) {
+      //     console.log('success');
+      //     return;
+      //   } else {
+      //     console.log('error: ', err);
+      //   }
+      // });
+      return new Promise( (resolve, reject) => {
+        fs.writeFile(writeFilePath, results, (err, res) => {
+          if (!err) {
+            resolve(res);
+          } else {
+            reject(err);
+          }
+        });
+      });
+    })
+    .catch( (error) => {
+      console.log('error: ', error);
+    });
 };
-
 // Export these functions so we can test them
 module.exports = {
   fetchProfileAndWriteToFile: fetchProfileAndWriteToFile
